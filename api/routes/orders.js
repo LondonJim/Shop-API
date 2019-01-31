36669d38428947721ceff3bb15a0b31a5e621656
James Sutherland <jrsutherland78@googlemail.com>
2019-01-31 12:47:25 +0000

    add order routes, add to app.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,1 +1,28 @@
+const express = require('express')
+const router = express.Router()
 
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders fetched'
+  })
+})
+
+router.post('/', (req, res, next) => {
+  res.status(201).json({
+    message: 'Orders created'
+  })
+})
+
+router.get('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Individual order details'
+  })
+})
+
+router.delete('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Individual order deleted'
+  })
+})
+
+module.exports = router
