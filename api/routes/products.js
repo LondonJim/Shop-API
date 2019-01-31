8bf97d59ff54d8fee7b326922d38c4a32a66267a
James Sutherland <jrsutherland78@googlemail.com>
2019-01-31 12:43:29 +0000

    add all product routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,36 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/')
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handle GET requests to /products'
+  })
+})
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handle POST requests to /products'
+  })
+})
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId
+  res.status(200).json({
+    message: 'product id passed',
+    id: id
+  })
+})
+
+router.patch('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'product is updated'
+  })
+})
+
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'product is deleted'
+  })
+})
+
+module.exports = router
