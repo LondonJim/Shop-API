ef2aae6d663e63f75abbd55613031934efb610d9
James Sutherland <jrsutherland78@googlemail.com>
2019-02-03 19:34:10 +0000

    add users, create and delete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/users')
 
 mongoose.connect("mongodb://" + process.env.MONGO_ATLAS_USER + ":" + process.env.MONGO_ATLAS_PW + "@node-shop-shard-00-00-xesbf.azure.mongodb.net:27017,node-shop-shard-00-01-xesbf.azure.mongodb.net:27017,node-shop-shard-00-02-xesbf.azure.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-shard-0&authSource=admin&retryWrites=true",
   {
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
+app.use('/users', userRoutes)
 
 app.use((req, res, next) => {
   const error = new Error('not found')
