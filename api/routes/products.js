3a081b0f3e83d7b136f1aad62b84cb98b541369d
James Sutherland <jrsutherland78@googlemail.com>
2019-02-03 21:21:51 +0000

    add checkAuth to product patch and delete

--- api/routes/products.js
+++ api/routes/products.js
@@ -116,7 +116,7 @@ router.get('/:productId', (req, res, next) => {
     })
 })
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
   const id = req.params.productId
   const updateOps = {}
   for (const ops of req.body) {
@@ -142,7 +142,7 @@ router.patch('/:productId', (req, res, next) => {
     })
 })
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
   const id = req.params.productId
   Product.remove({_id: id})
     .exec()
