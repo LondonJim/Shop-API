254b8a902589f8132ba025320abdc6b1e7b36b83
James Sutherland <jrsutherland78@googlemail.com>
2019-02-03 15:31:55 +0000

    add populate orders with product details

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,6 +10,7 @@ const Product = require('../models/product')
 router.get('/', (req, res, next) => {
   Order.find()
     .select('product quantity _id')
+    .populate('product', 'name price _id')
     .exec()
     .then(docs => {
       res.status(200).json({
@@ -69,6 +70,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
   Order.findById(req.params.orderId)
+    .populate('product', 'name price _id')
     .exec()
     .then(order => {
       res.status(200).json({
