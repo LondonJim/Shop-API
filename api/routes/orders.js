499c97c323dd4b6545852ae86074946faf4c4314
James Sutherland <jrsutherland78@googlemail.com>
2019-02-03 15:26:34 +0000

    add orders json returns get, post, delete

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,37 +1,111 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const serverURL = 'http://localhost:3000'
+
+const Order = require('../models/order')
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders fetched'
-  })
+  Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: 'GET',
+              url: serverURL + '/orders/' + doc._id
+            }
+          }
+        })
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  }
-  res.status(201).json({
-    message: 'Orders created',
-    order: order
-  })
+  Product.findById(req.body.productId)
+    .then(product => {
+      const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+      })
+      return order.save()
+    })
+    .then(result => {
+      console.log(result)
+      res.status(201).json({
+        message: 'Order stored',
+        createdOrder: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity
+        },
+        request: {
+          type: 'GET',
+          url: serverURL + '/orders/' + result._id
+        }
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Product not found',
+        error: err
+      })
+    })
 })
 
 router.get('/:orderId', (req, res, next) => {
-  const id = req.params.orderId
-  res.status(200).json({
-    message: 'Individual order details',
-    id: id
-  })
+  Order.findById(req.params.orderId)
+    .exec()
+    .then(order => {
+      res.status(200).json({
+        order: order,
+        request: {
+          type: 'GET',
+          url: serverURL + '/orders'
+        }
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Order not found',
+        error: err
+      })
+    })
 })
 
 router.delete('/:orderId', (req, res, next) => {
-  const id = req.params.orderId
-  res.status(200).json({
-    message: 'Individual order deleted',
-    id: id
-  })
+  Order.remove({ _id: req.params.orderId})
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Order deleted',
+        request: {
+          type: 'POST',
+          url: serverURL + '/orders',
+          body: { productId: 'ID', quantity: 'Number'}
+        }
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Order not found',
+        error: err
+      })
+    })
 })
 
 module.exports = router
